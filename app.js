6ef59666eef1fb7374aa72b8770de2faa10f2b80
Naim-Rana <naimrana015@gmail.com>
2022-02-14 11:41:57 +0600

    add product phone plus & minus button

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
-function updateProductNumber(product,isIncreasing){
+function updateProductNumber(product,price,isIncreasing){
     const productInput = document.getElementById(product+"-number");
-    const productNumber = productInput.value;
+    let productNumber = productInput.value;
     if(isIncreasing == true){
-        productInput.value =parseInt(productNumber)+ 1;
+        productNumber =parseInt(productNumber)+ 1;
     }
     else if(productNumber > 0){
-        productInput.value = parseInt(productNumber) - 1;
+        productNumber= parseInt(productNumber) - 1;
     }
+    productInput.value = productNumber;
+    // update product-total
+    const productTotal = document.getElementById(product+"-total");
+    productTotal.innerText = productNumber * price;
 }
+// product case-plus button
 document.getElementById("case-plus").addEventListener("click",function(){
-    updateProductNumber("case",true);
+    updateProductNumber("case",59,true);
 });
-
+// product case-minus button
 document.getElementById("case-minus").addEventListener("click",function(){
-    updateProductNumber("case",false);
-})
+    updateProductNumber("case",59,false);
+});
+// product phone-plus button
+document.getElementById("phone-plus").addEventListener("click",function(){
+    console.log("clicked phone-plus button");
+    updateProductNumber("phone",1219,true);
+});
+// product phone-minus button
+document.getElementById("phone-minus").addEventListener("click",function(){
+    console.log("clicked phone-minus button");
+    updateProductNumber("phone",1219,false);
+});
