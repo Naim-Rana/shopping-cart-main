a7eb51f88f7dcda5fdd207df488c2357ca609408
Naim-Rana <naimrana015@gmail.com>
2022-02-14 12:30:17 +0600

    add total price

--- app.js
+++ app.js
@@ -11,7 +11,35 @@ function updateProductNumber(product,price,isIncreasing){
     // update product-total
     const productTotal = document.getElementById(product+"-total");
     productTotal.innerText = productNumber * price;
+    // update calculate total
+    calculateTotal();
+    
 }
+function getInputValue(product){
+    const productInput = document.getElementById(product+"-number");
+    const productNumber = parseInt(productInput.value);
+    return productNumber;
+}
+
+function calculateTotal(){  
+    const phoneTotal = getInputValue("phone") * 1219;
+    const caseTotal = getInputValue("case") * 59;
+    const subtotal = phoneTotal + caseTotal;
+    const tax = subtotal /10;
+    const totalPrice = subtotal + tax ;
+    // update on the html subtotal
+    document.getElementById("sub-total").innerText =subtotal;
+    document.getElementById("tax-amount").innerText =tax;
+    document.getElementById("total-price").innerText =totalPrice;
+}
+// product phone-plus button
+document.getElementById("phone-plus").addEventListener("click",function(){
+    updateProductNumber("phone",1219,true);
+});
+// product phone-minus button
+document.getElementById("phone-minus").addEventListener("click",function(){
+    updateProductNumber("phone",1219,false);
+});
 // product case-plus button
 document.getElementById("case-plus").addEventListener("click",function(){
     updateProductNumber("case",59,true);
@@ -19,14 +47,5 @@ document.getElementById("case-plus").addEventListener("click",function(){
 // product case-minus button
 document.getElementById("case-minus").addEventListener("click",function(){
     updateProductNumber("case",59,false);
-});
-// product phone-plus button
-document.getElementById("phone-plus").addEventListener("click",function(){
-    console.log("clicked phone-plus button");
-    updateProductNumber("phone",1219,true);
-});
-// product phone-minus button
-document.getElementById("phone-minus").addEventListener("click",function(){
-    console.log("clicked phone-minus button");
-    updateProductNumber("phone",1219,false);
+    
 });
