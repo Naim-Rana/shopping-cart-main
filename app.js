4c31d1d2ad97e6fe7877432364c7670c865971c5
Naim-Rana <naimrana015@gmail.com>
2022-02-14 10:58:46 +0600

    added case minus event handelar

--- app.js
+++ app.js
@@ -3,4 +3,11 @@ document.getElementById("case-plus").addEventListener("click",function(){
     const caseInput = document.getElementById("case-number");
     const caseNumber = caseInput.value;
     caseInput.value =parseInt(caseNumber)+ 1;
+});
+
+document.getElementById("case-minus").addEventListener("click",function(){
+    console.log("clicked case-minus button ");
+    const caseInput = document.getElementById("case-number");
+    const caseNumber = caseInput.value;
+    caseInput.value = parseInt(caseNumber)-1;
 })
